6b5ce982a10d3dc0a0cdde82881020d1cc8fa11b
busticated <itsbusticated@gmail.com>
2011-04-24 19:42:43 -0700

    Indendation cleanup

--- public/js/main.js
+++ public/js/main.js
@@ -5,10 +5,10 @@ require( {
     }
   },[
     "jquery",
-		"modules/utils",
+    "modules/utils",
     "modules/guessWord",
     "libs/jquery.store"
-	], function($, utils, guessWord) {
+  ], function($, utils, guessWord) {
 
   // setup //////////////////////////////////////////////////////////
   $("h2.tabTitle").live("click", function() {
@@ -17,33 +17,33 @@ require( {
 
   // doc ready //////////////////////////////////////////////////////
   $(document).ready(function() {
-		var lastVisit = $.fn.store("get", "lastVisit");
+    var lastVisit = $.fn.store("get", "lastVisit");
 
-    //close tabs
+    //open "about" tab
     utils.tabDrawer("#info .tabTitle")
 
-		//handle new vs returning visitors
-		if (lastVisit) {
-			guessWord.init({
-				$target : $("#title"),
-				mode : "simple",
-				cb : function(){
-					setTimeout(guessWord.start, 2000);
-				}
-			});
-		} else {
-			guessWord.init({
-				$target : $("#title"),
-				mode : "full",
-				cb : function(){
-					setTimeout(function(){
-						guessWord.init({ mode:"simple" });
-					}, 2000);
-				}
-			});
-		}
-		
-		//set visit timestamp
-		$.fn.store("set", {"lastVisit": Date.now() });
+    //handle new vs returning visitors
+    if (lastVisit) {
+      guessWord.init({
+        $target : $("#title"),
+        mode : "simple",
+        cb : function(){
+          setTimeout(guessWord.start, 2000);
+        }
+      });
+    } else {
+      guessWord.init({
+        $target : $("#title"),
+        mode : "full",
+        cb : function(){
+          setTimeout(function(){
+            guessWord.init({ mode:"simple" });
+          }, 2000);
+        }
+      });
+    }
+    
+    //set visit timestamp
+    $.fn.store("set", {"lastVisit": Date.now() });
   });
 });
